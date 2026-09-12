ebf21c71f15e01fcadbb9cb4ebc9522de78dd225
Fatima Olea <232623523+fatimaov@users.noreply.github.com>
2026-09-12 20:34:42 +0200

    refactor: update style files structure + add color tokens

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+import './styles/variables.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import './styles/bootstrap.css'
+import './styles/bootstrap-overrides.css'
 import './styles/globals.css'
 import { router } from './router/router'
 
